64e05bc248cf826c44855b32e26efa5b0cd72dcf
MartinXCVI <mdzg96@gmail.com>
2024-05-30 16:22:24 -0300

    API reloj 'World Time API'

---
+++ js/apis/reloj.js
@@ -0,0 +1,19 @@
+async function obtenerHoraDelServidor() {
+  try {
+      const respuesta = await fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires');
+      const datos = await respuesta.json();
+      return datos.datetime.slice(11, 19); // Extraer solo la hora (HH:MM:SS)
+  } catch (error) {
+      console.error('Error al obtener la hora del servidor:', error);
+      return 'Error';
+  }
+}
+
+async function actualizarReloj() {
+  const hora = await obtenerHoraDelServidor();
+  const reloj = document.getElementById('reloj');
+  reloj.textContent = hora;
+}
+
+setInterval(actualizarReloj, 1000);
+actualizarReloj();
